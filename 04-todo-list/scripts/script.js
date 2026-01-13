b03a6763272333024071d86c1ba1f799dc27c85b
Elisson <elisson3004@gmail.com>
2026-01-13 13:19:06 -0300

    feat: add new project todo-list

---
+++ 04-todo-list/scripts/script.js
@@ -0,0 +1,79 @@
+const tasksContainer = document.getElementById('tasks-container');
+const input = document.getElementById('task-input');
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+createTasks()
+
+// Rotate input placeholder value
+const placeholders = ["Ler 10 páginas de um livro.", "Limpar a casa.", "Beber água."];
+let placeholder_index = 0;
+setInterval(() => {
+  placeholder_index = (placeholder_index + 1) % placeholders.length;
+  input.placeholder = placeholders[placeholder_index];
+}, 2400);
+
+// Add task when 'Enter' is pressed
+input.addEventListener('keydown', (event) => {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+
+function persistTasks(){
+    tasksString = JSON.stringify(tasks);
+    localStorage.setItem('tasks', tasksString);
+}
+
+function addTask() {
+    description = input.value;
+    if (!description) return;
+    
+    input.value = '';
+    tasks.push({ description, isDone: false});
+    createTasks();
+    persistTasks();
+}
+
+function deleteTask(index) {
+    tasks.splice(index, index+1);
+    document.getElementById(`task-${index}`).remove();
+    persistTasks();
+}
+
+function toogleTask(index) {
+    const className = "is-done";
+    const taskContainerEl = document.getElementById(`task-${index}`);
+    const checkButtonEl = taskContainerEl.firstElementChild;
+
+    tasks[index].isDone = !tasks[index].isDone;
+
+    if (tasks[index].isDone) {
+        taskContainerEl.classList.add(className);
+        checkButtonEl.firstElementChild.innerHTML = 'check_circle';
+    } else {
+        taskContainerEl.classList.remove(className)
+        checkButtonEl.firstElementChild.innerHTML = 'circle';
+    }
+}
+
+function createTasks(){
+    tasksContainer.innerHTML = '';
+    checkIcon = '<span class="material-symbols-outlined">check_circle</span>';
+    unchekIcon = '<span class="material-symbols-outlined">circle</span>';
+
+    for (i=0; i<tasks.length; i++){
+        const { description, isDone } = tasks[i];
+        const taskContainer = `
+            <div id="task-${i}" class="task">
+                <button id="check-button" class="check-button ${isDone ? 'is-done' : ''}" onclick="toogleTask(${i})">
+                    ${isDone ? checkIcon : unchekIcon}
+                </button>
+                <p ${isDone ? 'class="line-through"' : ''}>${description}</p>
+                <button class="delete-task-button" onclick="deleteTask(${i})">
+                    <span class="material-symbols-outlined">close_small</span>
+                </button>
+            </div>
+        `;
+        tasksContainer.innerHTML += taskContainer;
+    }
+}
