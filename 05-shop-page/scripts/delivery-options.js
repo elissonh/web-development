9d3c741f61e65c4534bd0de089837b7aebfb63e8
Elisson <elisson3004@gmail.com>
2026-01-19 22:32:11 -0300

    feat: add delivery options controller

---
+++ 05-shop-page/scripts/delivery-options.js
@@ -0,0 +1,14 @@
+export const deliveryOptions = {
+    1: {
+        days: 0,
+        priceCents: 0
+    },
+    2: {
+        days: 3,
+        priceCents: 499
+    },
+    3: {
+        days: 7,
+        priceCents: 999
+    }
+}
